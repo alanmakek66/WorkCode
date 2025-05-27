9615fbe3246447825598b0184956b324cf1e4303
alanmakek66 <164827200+alanmakek66@users.noreply.github.com>
2025-05-27 22:16:31 +0800

    Delete 0321 directory

--- 0321/app/src/main/java/com/example/a0321/DataCallback.java
+++
@@ -1,9 +0,0 @@
-package com.example.a0321;
-
-import java.util.List;
-
-public interface DataCallback {
-    void onDataFetched(List<FqaModel> fqaModels);
-
-    void onDataFetched2(List<TranslateList> translateLists);
-}
--- 0321/app/src/main/java/com/example/a0321/DataFetchCallback.java
+++
@@ -1,7 +0,0 @@
-package com.example.a0321;
-
-import java.util.List;
-
-public interface DataFetchCallback {
-    void onDataFetched(List<TranslateList> data);
-}
